f6240db797ff7b1873a59d0dc233eb94defc996e
christianalim <christianalim1@gmail.com>
2019-08-14 17:24:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(array, fn){
+    let newArray = []
+    array.forEach(thing => newArray.push(fn(thing)))
+    return newArray
+}
+
+function reduce(array, fn, start = 0){
+    let r = (!!start)? start: array[0]
+    let i = (!!start)? 0:1
+    for(;i < array.length; i++){
+        r = fn(array[i], r)
+    }
+    return r
+}
