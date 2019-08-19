0875335998417923a353b3adb55807dbd4d8f6ff
github username <github email address>
2019-08-18 20:15:12 -0500

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function map(array, callback) {
 
 
   function reduce(source, callback, starting){
-    let r = (starting) ? starting : source[0] // if start point is greater than 0, then start at that number, otherwise start at the first number in the array
-    let i = (starting) ? 0 : 1 // if there is a starting number, then add everything from index 0, otherwise add from index 1 and use index 0 as the starting point
+    let r = (starting) ? starting : source[0] 
+    // start at starting point if it is >0, else start at 0
+    let i = (starting) ? 0 : 1
+     //if start # is present, then add everything to index 0, else add index 1 and with index 0 as the starting point
   
     for (; i < source.length; i++) {
-      r = callback(source[i], r) //pass through each element and the running total
+      r = callback(source[i], r) 
     }
   
     return r;
